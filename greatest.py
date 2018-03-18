493f85dabd5ebc128483569118ecaaa929402849
AnusriChaurasia <35715688+AnusriChaurasia@users.noreply.github.com>
2018-03-18 18:22:42 +0530

    Add files via upload

---
+++ greatest.py
@@ -0,0 +1,10 @@
+a=(int(input("enter a number:")))
+b=int(input("enter a number:"))
+c=int(input("enter a number:"))
+if a>b and a>c:
+    print(str(a)+"is greatest")
+elif b>c:
+    print(str(b)+"is greatest")
+else:
+    print(str(c)+"is greatest")
+    
